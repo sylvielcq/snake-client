224c7c86c41476a608c1ac658d9454dcb52002fd
sylvielcq <slcq.dev@gmail.com>
2022-08-03 20:00:41 -0700

    Created a constants module

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 const net = require("net");
+const { IP, PORT, name } = require("./constants");
 
-const name = 'SLE';
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
---
+++ constants.js
@@ -0,0 +1,11 @@
+const IP = '165.227.47.243';
+const PORT = 50541;
+const name = 'SLE';
+
+
+
+module.exports = {
+  IP,
+  PORT,
+  name
+};
